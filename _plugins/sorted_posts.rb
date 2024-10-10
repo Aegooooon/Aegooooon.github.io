369b3693f8d0862f0d2542b775dae431c8762763
陈思元 <143163821+ccccsy04@users.noreply.github.com>
2024-10-10 20:02:00 +0800

    resume

--- _plugins/sorted_posts.rb
+++ _plugins/sorted_posts.rb
@@ -1,15 +1,12 @@
 module Jekyll
-      class SortedPostsGenerator < Generator
-            safe true
-        
-            def generate(site)
-              all_posts = site.posts.docs
-              high_priority_posts = all_posts.reject { |post| post.data['priority'] == 'low' }
-              low_priority_posts = all_posts.select { |post| post.data['priority'] == 'low' }
-              sorted_posts = high_priority_posts + low_priority_posts
-        
-              # 更新 site 的 posts 以便分页插件使用
-              site.posts.docs.replace(sorted_posts)
-            end
+      class LowPriorityLast < Generator
+        safe true
+        priority :lowest
+    
+        def generate(site)
+          site.posts.docs.sort_by! do |post|
+            post.data['priority'] == 'low' ? 1 : 0
           end
         end
+      end
+    end
